8201581b2080e5526ccc8ce30aacbf2b1ec05049
Peter Cheng <petercheng7788@gmail.com>
2020-08-19 15:59:51 +0800

    test the reuse function

--- server.js
+++ server.js
@@ -3,6 +3,26 @@ const express = require("express");
 const cors = require("cors");
 const stripe = require("stripe")(process.env.STIPE_KEY);
 
+const test = async () => {
+  const paymentMethods = await stripe.paymentMethods.list({
+    customer: "cus_HrBbGgXLtcrCC0",
+    type: "card",
+  });
+  console.log(paymentMethods);
+  console.log(paymentMethods.data[0].billing_details);
+  console.log(paymentMethods.data[0].card);
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: 500,
+    currency: "hkd",
+    customer: "cus_HrBbGgXLtcrCC0",
+    payment_method: "pm_1HHTE2IEWLQJp0xomC8M0F7B",
+    off_session: true,
+    confirm: true,
+  });
+  console.log(paymentIntent)
+  console.log(paymentIntent.client_secret);
+};
+
 const bodyParser = require("body-parser");
 const app = express();
 app.use(
@@ -45,6 +65,7 @@ app.post("/create-setup-intent", async (req, res) => {
 
 app.listen(process.env.PORT, function () {
   console.log("Server Start");
+  test();
 });
 
 // app.use(express.static("."));
